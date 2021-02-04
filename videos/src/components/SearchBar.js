343393338af279961096f01272cfc5d9adfc155c
SC <inthetelevision@gmail.com>
2021-02-04 22:23:13 +0900

    private work?

---
+++ videos/src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    state = {term: ''};
+
+    onInputChange = (event) => {
+        this.setState({term: event.target.value});
+    };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        
+    }
+
+    render() {
+        return (
+            <div className="search-bar ui segment">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input type="text" onChange={this.onInputChange} value={this.state.term}/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
