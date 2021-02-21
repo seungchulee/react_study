0d0f91594f462a2e2777dfefa0607aa7f4f63a12
SC <inthetelevision@gmail.com>
2021-02-21 18:37:14 +0900

    Understanding Hooks in React - 4

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from './components/Accordion'
 import Search from './components/Search'
 import Dropdown from './components/Dropdown'
+import Translate from './components/Translate'
 
 const items = [
     {
@@ -34,11 +35,17 @@ const options = [
 ]
 
 export default () => {
+    // const [selected, setSelected] = useState(options[0]);
+    // const [showDropdown, setShowDropdown] = useState(true);
     return (
         <div>
             {/* <Accordion items={items}/> */}
             {/* <Search/> */}
-            <Dropdown options={options}/>
+            {/* <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+            {
+                showDropdown ? <Dropdown options={options} selected={selected} onSelectedChange={setSelected}/> : null
+            } */}
+            <Translate/>
         </div>
     )
 }
--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-const Dropdown = ({options}) => {
+const Dropdown = ({label, options, selected, onSelectedChange}) => {
+    const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+        const bodyClick = (event) => {
+            if (ref.current && ref.current.contains(event.target)){
+                return;
+            }
+            setOpen(false);
+        }
+        document.body.addEventListener('click', bodyClick, {capture: true});
+        return () => {
+            document.body.removeEventListener('click', bodyClick)
+        }
+    }, []);
+
     const renderedOptions = options.map((option) => {
+        if (option.value === selected.value){
+            return null;
+        }
         return (
-            <div key={option.value} className="item">
+            <div key={option.value} className="item" onClick={() => onSelectedChange(option)}>
                 {option.label}
             </div>
         )
     })
 
     return (
-        <div className="ui form">
+        <div className="ui form" ref={ref}>
             <div className="field">
-                <label className="label">Select a Color</label>
-                <div className="ui selection dropdown visible active">
+                <label className="label">{label}</label>
+                <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
                     <i className="dropdown icon"></i>
-                    <div className="text">Select Color</div>
-                    <div className="menu visible transition">
+                    <div className="text">{selected.label}</div>
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>
                         {renderedOptions}
                     </div>
                 </div>
