65d22f4757a3f84e8a4c7910fbd724982dcb2cb4
SC <inthetelevision@gmail.com>
2021-03-18 23:55:58 +0900

    Redux Store Design - 1

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -5,7 +5,17 @@ export const fetchPosts = () => {
         const response = await jsonPlaceholder.get('/posts');
         dispatch({
             type:'FETCH_POSTS',
-            payload: response
+            payload: response.data
+        })
+    }
+}
+
+export const fetchUser = (id) => {
+    return async (dispatch) => {
+        const response = await jsonPlaceholder.get(`/users/${id}`);
+        dispatch({
+            type: 'FETCH_USER',
+            payload: response.data
         })
     }
 }
--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,7 +1,6 @@
 import {combineReducers} from 'redux';
-
-
+import postReducer from './postReducer';
 
 export default combineReducers({
-    replaceMe: () => 'hi'
+    posts: postReducer
 });
