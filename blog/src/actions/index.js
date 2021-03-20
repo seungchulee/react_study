00dd075a415cf92848f17b64dcb133e48517509d
SC <inthetelevision@gmail.com>
2021-03-20 21:41:16 +0900

    Redux Store Design - 2

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
@@ -10,6 +11,20 @@ export const fetchPosts = () => {
     }
 }
 
+// export const fetchUser = (id) => {
+//     return (dispatch) => {
+//         _fetchUser(id, dispatch);
+//     }
+// }
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data
+//     })
+// })
+
 export const fetchUser = (id) => {
     return async (dispatch) => {
         const response = await jsonPlaceholder.get(`/users/${id}`);
@@ -18,4 +33,16 @@ export const fetchUser = (id) => {
             payload: response.data
         })
     }
+}
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'))
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
 }
--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -3,16 +3,22 @@ import { connect } from 'react-redux';
 import {fetchUser} from '../actions'
 
 class UserHeader extends React.Component {
-    componentDidMount() {
-        this.props.fetchUser(this.props.userId);
-    }
 
     render() {
+        const { user } = this.props
+        if (!user){
+            return null;
+        }
         return (
-            <div>
-                User Header;
+            <div className="header">
+                {user.name}        
             </div>
         )
     }
 }
-export default connect(null, {fetchUser})(UserHeader);
+
+const mapStateToProps = (state, ownProps) => {
+    return {user: state.users.find(user => user.id === ownProps.userId)}
+}
+
+export default connect(mapStateToProps)(UserHeader);
